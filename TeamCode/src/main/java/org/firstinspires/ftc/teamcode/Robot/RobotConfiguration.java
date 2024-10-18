64e9c223e3eb94cf42e55746ea27a983dabbcf4b
dwheatle <wheatfrc@gmail.com>
2024-10-18 15:54:03 -0400

    Formatting

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/RobotConfiguration.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/RobotConfiguration.java
@@ -40,12 +40,12 @@ public abstract class RobotConfiguration extends LinearOpMode {
 
 
     /*----------- Define all Module Classes (SubSystems) -----------*/
-    protected MecanumDriveBasic drive = null;
-    protected MecanumDrive autoDrive = null;
+    protected MecanumDriveBasic  drive = null;
+    protected MecanumDrive       autoDrive = null;
     protected VisionPortalObject vision = null;
-    protected Servo1D WristRotate;
-    protected Servo1D WristPivot;
-    protected Servo1D Gripper;
+    protected Servo1D            WristRotate;
+    protected Servo1D            WristPivot;
+    protected Servo1D            Gripper;
     /* keyword 'protected' is similar to 'private'. Private variables are only accessible within the
     scope of this class. Protected allows for all classes that extend or inherit from this class to
     directly access the variables, objects and methods. */
@@ -73,10 +73,10 @@ public abstract class RobotConfiguration extends LinearOpMode {
         DcMotorEx driveMotorLR = hardwareMap.get(DcMotorEx.class, "leftRear");
         DcMotorEx driveMotorRR = hardwareMap.get(DcMotorEx.class, "rightRear");
 
-        WebcamName webCam  = hardwareMap.get(WebcamName.class, "Webcam 1");
-        Servo wristPivotServo = hardwareMap.get(Servo.class, "wristPivotServo");
+        WebcamName webCam      = hardwareMap.get(WebcamName.class, "Webcam 1");
+        Servo wristPivotServo  = hardwareMap.get(Servo.class, "wristPivotServo");
         Servo wristRotateServo = hardwareMap.get(Servo.class, "wristRotateServo");
-        Servo gripperServo = hardwareMap.get(Servo.class, "gripperServo");
+        Servo gripperServo     = hardwareMap.get(Servo.class, "gripperServo");
 
         /** Create an object of every module/subsystem needed for both autonomous and teleOp modes. **/
         drive       = new MecanumDriveBasic(driveMotorLF, driveMotorLR, driveMotorRF, driveMotorRR);
