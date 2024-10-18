c82535549fdef92d84598995a59816ed2cb6184a
dwheatle <wheatfrc@gmail.com>
2024-10-18 18:58:01 -0400

    removed experimental code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes_Autonomous/AutoRR.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes_Autonomous/AutoRR.java
@@ -40,8 +40,6 @@ public class AutoRR extends RobotConfiguration implements TeamConstants {
 
         while (opModeIsActive()) {
 
-            gripper.setPosition(0.87);
-            gripper.toggleGripper();
         }
     }
 }
