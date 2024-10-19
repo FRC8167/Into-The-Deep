d8aa4eb6942593e6589e84d6c2d837aba0d59e1b
dwheatle <wheatfrc@gmail.com>
2024-10-19 14:11:56 -0400

    Added notes to TeamConstans

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/TeamConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/TeamConstants.java
@@ -11,19 +11,22 @@ public interface TeamConstants {
 
 
     /*~~~~~~~~~~~~~~~~~~~~~ Wrist Pivot Subsystem Constants ~~~~~~~~~~~~~~~~~~~~~*/
-    double WRIST_PIVOT_MIN = 0.0;
-    double WRIST_PIVOT_MAX = 1.0;
+    double WRIST_PIVOT_MIN    = 0.0;
+    double WRIST_PIVOT_MAX    = 1.0;
     double WRIST_PIVOT_CENTER = 0.5;
+    // Need preset positions
 
 
     /*~~~~~~~~~~~~~~~~~~~~ Wrist Rotate Subsystem Constants ~~~~~~~~~~~~~~~~~~~~*/
-    double WRIST_ROTATE_MIN = 0.5;
-    double WRIST_ROTATE_MAX = 0.0;
+    double WRIST_ROTATE_MIN    = 0.5;
+    double WRIST_ROTATE_MAX    = 0.0;
     double WRIST_ROTATE_CENTER = 0.25;  // Needs Verified
 
 
     /*~~~~~~~~~~~~~~~~~~~~~~~ Gripper Subsystem Constants ~~~~~~~~~~~~~~~~~~~~~~~*/
-    double GRIPPER_CLOSE = 0.515;
-    double GRIPPER_OPEN  = 0.45;
+    double GRIPPER_MAX_POS = 0.515;
+    double GRIPPER_MIN_POS = 0.40;
+    double GRIPPER_CLOSE   = 0.51;
+    double GRIPPER_OPEN    = 0.45;
 
 }
