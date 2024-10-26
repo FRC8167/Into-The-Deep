32330a4574eca045363b2eedca75192328571e15
dwheatle <wheatfrc@gmail.com>
2024-10-26 16:16:36 -0400

    Minor Formatting

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SubSytems/ServoToggle.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SubSytems/ServoToggle.java
@@ -3,9 +3,12 @@ package org.firstinspires.ftc.teamcode.SubSytems;
 import com.qualcomm.robotcore.hardware.Servo;
 
 public class ServoToggle extends Servo1D {
+
+
     public ServoToggle(Servo servo, double initPos, double min, double max) {
         super(servo, initPos, min, max);
     }
+
 
     public void toggleGripper(){
 
