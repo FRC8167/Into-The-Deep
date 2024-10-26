9c981abfec7e6a6b0d891092e5c47e637f4acf51
dwheatle <wheatfrc@gmail.com>
2024-10-26 16:53:39 -0400

    Added TODO comments

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/TeamConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/TeamConstants.java
@@ -6,15 +6,14 @@ public interface TeamConstants {
     double DEGRADED_DRIVE_LIMIT    = 0.45;
     double DEGRADED_STRAFE_LIMIT   = 0.35;
     double DEGRADED_TURN_LIMIT     = 0.25;
-    double DEGRADED_SLIDE_EXTENDED = 10000;
-    double DEGRADED_ARM_ROTATION   = 10000;
+    double DEGRADED_SLIDE_EXTENDED = 10000; // TODO: update when the slide install is complete
+    double DEGRADED_ARM_ROTATION   = 10000; // TODO: update when the arm install is complete
 
 
     /*~~~~~~~~~~~~~~~~~~~~~ Wrist Pivot Subsystem Constants ~~~~~~~~~~~~~~~~~~~~~*/
     double PIVOT_MIN = 0.0;
     double PIVOT_MAX = 1.0;
     double PIVOT_CENTER = 0.5;
-
     // Need preset positions
 
 
@@ -31,7 +30,9 @@ public interface TeamConstants {
     double GRIPPER_OPEN    = 0.45;
 
     /*~~~~~~~~~~~~~~~~~~~~~ Arm Rotate Subsystem Constants ~~~~~~~~~~~~~~~~~~~~~~*/
-    double DEGREES_TO_COUNTS = 1.0; // Needs updated when Motor/Gearing is determined
-    int MIN_POSITION_COUNTS = 0;    // Needs updated when the arm install is complete
-    int MAX_POSITION_COUNTS = 0;    // Needs updated when the arm install is complete
+    double DEGREES_TO_COUNTS = 1.0; // TODO: update when Motor/Gearing is determined
+    int MIN_POSITION_COUNTS = 0;    // TODO: update when the arm install is complete
+    int MAX_POSITION_COUNTS = 0;    // TODO: update when the arm install is complete
+
+
 }
