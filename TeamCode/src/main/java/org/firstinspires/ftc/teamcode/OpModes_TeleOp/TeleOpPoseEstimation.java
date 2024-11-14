550a6f0c8ba263b7eea36f7e54a9b56942632f5c
dwheatle <wheatfrc@gmail.com>
2024-11-14 15:50:11 -0500

    Pose Estimate opMode complete

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes_TeleOp/TeleOpPoseEstimation.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes_TeleOp/TeleOpPoseEstimation.java
@@ -4,8 +4,10 @@ import com.acmerobotics.roadrunner.Pose2d;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+import org.firstinspires.ftc.teamcode.Cogintilities.GamepadWrapper;
 import org.firstinspires.ftc.teamcode.Robot.RobotConfiguration;
 import org.firstinspires.ftc.teamcode.Robot.TeamConstants;
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
 
 @Disabled
 @TeleOp(name="PoseEstimator", group="Competition")
@@ -17,12 +19,32 @@ public class TeleOpPoseEstimation extends RobotConfiguration implements TeamCons
         /* Position robot and the center of the play pen, heading facing 0 degrees */
         initializeRobot(new Pose2d(0,0,0));
 
+        GamepadWrapper driver = new GamepadWrapper(gamepad1);
+
         waitForStart();
 
         while (opModeIsActive()) {
 
-            drive.mecanumDrive(0,0,0);
+            drive.mecanumDrive(-driver.leftStick_Y, driver.leftStick_X, driver.rightStick_X);
 
+            aprilTags.scanForAprilTags();
+            if(aprilTags.aprilTagsDetected()) {
+                for(AprilTagDetection tag : aprilTags.allAprilTagsDetected()) {
+                    telemetry.addData("Tag ID: ", tag.id);
+                    telemetry.addData("X: ", tag.robotPose.getPosition().x);
+                    telemetry.addData("Y: ", tag.robotPose.getPosition().x);
+                    telemetry.addData("Heading", tag.robotPose.getOrientation().getYaw());
+                }
+            }
+
+            telemetry.addLine("\n_____________________________________\n");
+            telemetry.addLine("Road Runner Data");
+            telemetry.addData("X: ", autoDrive.pose.position.x);
+            telemetry.addData("Y: ", autoDrive.pose.position.y);
+            telemetry.addData("Heading: ", autoDrive.pose.heading);
+
+            telemetry.update();
+            driver.update();
         }
     }
 }
