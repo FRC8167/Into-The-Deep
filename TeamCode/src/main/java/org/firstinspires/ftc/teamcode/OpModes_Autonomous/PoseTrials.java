a6359121b6e106ac0dff1521a85972ff0a6a542b
dwalsh <dwalsh@rsd13.org>
2024-12-13 09:54:23 -0500

    Attempted pose mapping but completely by brute force which cannot be the intention.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes_Autonomous/PoseTrials.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes_Autonomous/PoseTrials.java
@@ -3,40 +3,59 @@ package org.firstinspires.ftc.teamcode.OpModes_Autonomous;
 import android.annotation.SuppressLint;
 
 import com.acmerobotics.roadrunner.Action;
-import com.acmerobotics.roadrunner.DualNum;
+import com.acmerobotics.roadrunner.HeadingPath;
 import com.acmerobotics.roadrunner.Pose2d;
-import com.acmerobotics.roadrunner.Pose2dDual;
 import com.acmerobotics.roadrunner.SequentialAction;
-import com.acmerobotics.roadrunner.Trajectory;
 import com.acmerobotics.roadrunner.TrajectoryActionBuilder;
-import com.acmerobotics.roadrunner.TrajectoryBuilder;
 import com.acmerobotics.roadrunner.TrajectoryBuilderParams;
 import com.acmerobotics.roadrunner.Vector2d;
 import com.acmerobotics.roadrunner.ftc.Actions;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
-import org.firstinspires.ftc.teamcode.MecanumDrive;
 import org.firstinspires.ftc.teamcode.Robot.RobotConfiguration;
 import org.firstinspires.ftc.teamcode.Robot.TeamConstants;
-import org.firstinspires.ftc.vision.opencv.ColorBlobLocatorProcessor;
-import org.opencv.core.RotatedRect;
 
 //@Disabled
 @Autonomous(name="AutoBlueFarSub", group="Autonomous", preselectTeleOp = "TeleOp")
-public class AutoBlueFarSub extends RobotConfiguration implements TeamConstants {
+public class PoseTrials extends RobotConfiguration implements TeamConstants {
+
+
+
+    Pose2d poseFlipper(Pose2d pose) {
+        return new Pose2d(
+                -pose.position.x,  //reflect x-coordinate
+                -pose.position.y,       //reflect y-coordinate
+                pose.heading.real + Math.PI //  Add pi to heading as I could not implement .inverse()
+        );
+    }
+
+    Vector2d vectorFlipper(Vector2d vector) {
+       return (new Vector2d(
+               -vector.x,
+               -vector.y));
+    }
+
+    double headingFlipper(double heading) {
+        return (heading + Math.PI);
+    }
+
+    int lineToFlipper(int ordinate) {
+        return (-ordinate);
+    }
+
 
     @SuppressLint("DefaultLocale")
     @Override
     public void runOpMode() throws InterruptedException {
 
-        Pose2d initialPose = new Pose2d(-14,60, -Math.PI/2);
-
+        Pose2d initialPose = poseFlipper(new Pose2d(-14,60, -Math.PI/2));
         initializeRobot(initialPose);
-        setAlliance(AllianceColor.BLUE);
+        setAlliance(AllianceColor.RED);
+
 
         TrajectoryActionBuilder forward1 = autoDrive.actionBuilder(initialPose)
                 .lineToY(36);
-        TrajectoryActionBuilder block1 = autoDrive.actionBuilder(new Pose2d(-14, 36, -Math.PI/2))
+        TrajectoryActionBuilder block1 = autoDrive.actionBuilder(poseFlipper(new Pose2d(-14, 36, -Math.PI/2)))
                 .strafeTo(new Vector2d(50,36));
         TrajectoryActionBuilder basket1 =autoDrive.actionBuilder(new Pose2d(50, 36, -Math.PI/2))
                 .strafeToSplineHeading(new Vector2d(55,55),Math.toRadians(45));
@@ -51,7 +70,7 @@ public class AutoBlueFarSub extends RobotConfiguration implements TeamConstants 
         TrajectoryActionBuilder park = autoDrive.actionBuilder(new Pose2d(55, 55, Math.toRadians(45)))
                 .strafeToSplineHeading(new Vector2d(30,10),Math.toRadians(180));
 
-
+   
 
         waitForStart();
 
