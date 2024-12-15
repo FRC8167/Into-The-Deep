fa12441c9adf2561cd7e9014cffd8dee6c47d208
dwalsh <dwalsh@rsd13.org>
2024-12-15 14:08:29 -0500

    Look at PoseTrials and the TrajectoryActionBuilder in MecanumDrive which is now overloaded.  Is adding the parameter to the TrajectoryActionBuilder enough to map all of the .builder parts?
    Also, what about the initial pose???

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes_Autonomous/PoseTrials.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes_Autonomous/PoseTrials.java
@@ -7,10 +7,13 @@ import android.annotation.SuppressLint;
 import com.acmerobotics.roadrunner.Action;
 import com.acmerobotics.roadrunner.Arclength;
 import com.acmerobotics.roadrunner.HeadingPath;
+import com.acmerobotics.roadrunner.HeadingPosePath;
 import com.acmerobotics.roadrunner.Pose2d;
 import com.acmerobotics.roadrunner.Pose2dDual;
 import com.acmerobotics.roadrunner.PoseMap;
 import com.acmerobotics.roadrunner.ProfileParams;
+import com.acmerobotics.roadrunner.Rotation2d;
+import com.acmerobotics.roadrunner.Rotation2dDual;
 import com.acmerobotics.roadrunner.SequentialAction;
 import com.acmerobotics.roadrunner.TrajectoryActionBuilder;
 import com.acmerobotics.roadrunner.TrajectoryBuilder;
@@ -27,30 +30,43 @@ import org.firstinspires.ftc.teamcode.Robot.TeamConstants;
 @Autonomous(name="AutoBlueFarSub", group="Autonomous", preselectTeleOp = "TeleOp")
 public class PoseTrials extends RobotConfiguration implements TeamConstants {
 
+
+
     @SuppressLint("DefaultLocale")
     @Override
     public void runOpMode() throws InterruptedException {
 
+
+
+
+
+
         Pose2d initialPose = new Pose2d(-14,60, -Math.PI/2);
         initializeRobot(initialPose);
         setAlliance(AllianceColor.RED);
-        TrajectoryActionBuilder forward1 = autoDrive.actionBuilder(initialPose)
+        TrajectoryActionBuilder forward1 = autoDrive.actionBuilder(initialPose, p-> new Pose2dDual<>(p.position.x.unaryMinus(), p.position.y.unaryMinus(), p.heading.inverse()))
                 .lineToY(36);
-        TrajectoryActionBuilder block1 = autoDrive.actionBuilder(new Pose2d(-14, 36, -Math.PI/2))
+        TrajectoryActionBuilder block1 = autoDrive.actionBuilder(new Pose2d(-14, 36, -Math.PI/2), p-> new Pose2dDual<>(p.position.x.unaryMinus(), p.position.y.unaryMinus(), p.heading.inverse()))
                 .strafeTo(new Vector2d(50,36));
-        TrajectoryActionBuilder basket1 =autoDrive.actionBuilder(new Pose2d(50, 36, -Math.PI/2))
+        TrajectoryActionBuilder basket1 =autoDrive.actionBuilder(new Pose2d(50, 36, -Math.PI/2), p-> new Pose2dDual<>(p.position.x.unaryMinus(), p.position.y.unaryMinus(), p.heading.inverse()))
                 .strafeToSplineHeading(new Vector2d(55,55),Math.toRadians(45));
-        TrajectoryActionBuilder block2 = autoDrive.actionBuilder(new Pose2d(55, 55, Math.toRadians(45)))
-                .strafeToSplineHeading(new Vector2d(57.5,38),Math.toRadians(-90));
-        TrajectoryActionBuilder basket2 = autoDrive.actionBuilder(new Pose2d(57.5, 38, Math.toRadians(-90)))
+
+
+        TrajectoryActionBuilder block2 = autoDrive.actionBuilder(
+                        new Pose2d(55, 55, Math.toRadians(45)),
+                        p -> new Pose2dDual<>(p.position.x.unaryMinus(), p.position.y.unaryMinus(), p.heading.inverse()))
+                        .strafeToSplineHeading(new Vector2d(57.5, 38), Math.toRadians(-90));
+
+        TrajectoryActionBuilder basket2 = autoDrive.actionBuilder(new Pose2d(57.5, 38, Math.toRadians(-90)), p-> new Pose2dDual<>(p.position.x.unaryMinus(), p.position.y.unaryMinus(), p.heading.inverse()))
                 .strafeToSplineHeading(new Vector2d(55,55),Math.toRadians(45));
 
         TrajectoryActionBuilder block3 = autoDrive.actionBuilder(new Pose2d(55, 55, Math.toRadians(45)), p-> new Pose2dDual<>(p.position.x.unaryMinus(), p.position.y.unaryMinus(), p.heading.inverse()))
                 .strafeToSplineHeading(new Vector2d(55,25),Math.toRadians(0));
-        TrajectoryActionBuilder basket3 = autoDrive.actionBuilder(new Pose2d(55, 25, Math.toRadians(45)))
-                .strafeToSplineHeading(new Vector2d(55,55),Math.toRadians(45));
-        TrajectoryActionBuilder park = autoDrive.actionBuilder(new Pose2d(55, 55, Math.toRadians(45)))
-                .strafeToSplineHeading(new Vector2d(30,10),Math.toRadians(180));
+
+        TrajectoryActionBuilder basket3 = autoDrive.actionBuilder(new Pose2d(55, 25, Math.toRadians(45)), p-> new Pose2dDual<>(p.position.x.unaryMinus(), p.position.y.unaryMinus(), p.heading.inverse()))
+                .strafeToSplineHeading(new Vector2d(55, 55), Math.toRadians(45));
+        TrajectoryActionBuilder park = autoDrive.actionBuilder(new Pose2d(55, 55, Math.toRadians(45)), p -> new Pose2dDual<>(p.position.x.unaryMinus(), p.position.y.unaryMinus(), p.heading.inverse()))
+                .strafeToSplineHeading(new Vector2d(30, 10), Math.toRadians(180));
 
    
 
