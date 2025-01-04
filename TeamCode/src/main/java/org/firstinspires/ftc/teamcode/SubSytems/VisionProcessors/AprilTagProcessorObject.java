2c0944887c725133f11be4a7c7b204ca177ee971
dwalsh <dwalsh@rsd13.org>
2025-01-04 16:00:03 -0500

    yaw

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SubSytems/VisionProcessors/AprilTagProcessorObject.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SubSytems/VisionProcessors/AprilTagProcessorObject.java
@@ -37,7 +37,7 @@ public class AprilTagProcessorObject {
     private final double LENS_INTRINSICS_CY = 411.6;
 
     private Position cameraPosition = new Position(DistanceUnit.INCH,-1, 9, 5.5, 0);
-    private YawPitchRollAngles cameraOrientation = new YawPitchRollAngles(AngleUnit.DEGREES,-90,  0, 0, 0);
+    private YawPitchRollAngles cameraOrientation = new YawPitchRollAngles(AngleUnit.DEGREES,-90,  -90, 0, 0);
 
     /**
      * Create an processor for April Tag detections
