47ec601c4c76c57aca79382bb5812ea858948f89
emark <emarks0825@gmail.com>
2025-02-25 17:25:15 -0500

    Fixed TestAction, run ends when false not true

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SubSytems/TestAction.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SubSytems/TestAction.java
@@ -34,7 +34,7 @@ public class TestAction {
                 previousTime = currentTime - (currentTime - previousTime) % 1000;
             }
 
-            return count > target;
+            return !(count > target);
         }
 
     }
